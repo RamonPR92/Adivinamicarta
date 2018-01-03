7f0b18f186e671a2db706509fd17069643e934cb
RamonPR92 <ramonpr92@hotmail.com>
2018-01-03 14:45:58 -0600

    Se genera el metodo que recoge las cartas al ser elegida una columna, se contempla y se ordena la columna elegida en medio de las otras dos

--- app/src/main/java/com/perez/ramon/adivinamicarta/Baraja.java
+++ app/src/main/java/com/perez/ramon/adivinamicarta/Baraja.java
@@ -1,10 +1,68 @@
 package com.perez.ramon.adivinamicarta;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Ramon on 03/01/2018.
  */
 
 public class Baraja {
+
+
+    /**
+     * Recoge o fusiona tres arreglos pequeños en uno mas grande
+     * @param uno
+     * @param dos
+     * @param tres
+     * @param columnaElegida representa al array que debera quedar siempre en medio de los otros dos
+     * @return un array compuesto con los tres pasados como parametro, pero con la columna elegida, en medio de los otros dos arreglos
+     */
+    public int[] recogerCartas(int[] uno, int[]dos, int[]tres, int columnaElegida){
+
+        switch(columnaElegida){
+            case 1:
+                return ordenarColumna(dos,uno,tres);
+
+            case 2:
+                return ordenarColumna(uno, dos, tres);
+
+            default://En el caso que sea  la columna 3
+                return ordenarColumna(uno, tres, dos);
+        }
+    }
+
+    /**
+     * Ordena los tres arreglos en uno mas grande, pero ya estan pasados en un orden
+     * @param primero
+     * @param segundo
+     * @param tercero
+     * @return un array en el orden enviado
+     */
+    private int[] ordenarColumna(int [] primero, int[] segundo, int[] tercero){
+         int[] cartasOrdenadas = new int[9];
+         int posicionArrayPrincipal = 0;
+
+        for (int i = 0; i < cartasOrdenadas.length ; i++) {
+            for (int j = 0; j < primero.length; j++) {
+                cartasOrdenadas[posicionArrayPrincipal] = primero[j];
+                posicionArrayPrincipal++;//0 a 2
+            }
+            for (int j = 0; j < primero.length; j++) {
+                cartasOrdenadas[posicionArrayPrincipal] = segundo[j];
+                posicionArrayPrincipal++;// 3 a 5
+            }
+            for (int j = 0; j < primero.length; j++) {
+                cartasOrdenadas[posicionArrayPrincipal] = tercero[j];
+                posicionArrayPrincipal++;// 6 a 8
+            }
+        }
+        return cartasOrdenadas;
+    }
+
+    public int adivinarCarta(){
+        return  5;
+    }
 
     /**
      * Genera 9 numeros aleatorios entre 1 y 9 , sin repetirlos,
